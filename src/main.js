2483dda67539612930058a0765a6d47f0ead1933
GISTWJ <3238670575@qq.com>
2022-11-04 17:40:04 +0800

    git@github.com:3238670575/webmap.git

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入路由
+import router from '../router'
+// 引入vuex仓库
+import store from '../store'
 
 Vue.config.productionTip = false
 
 new Vue({
+  // 注册路由和仓库
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
